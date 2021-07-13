6d28746054792ebc78def50e4669092f1341423a
Bryanskaya <bryanskayakatya@yandex.ru>
2021-07-13 21:03:24 +0300

    CORRECTED: task1

--- task1/Form1.cs
+++ task1/Form1.cs
@@ -22,42 +22,79 @@ namespace task1
         private void do_task()
         {
             int lN = 1000;
-            double t = 0, dt = 1e-3;
+            double t = 0;
 
             double[] xArr = new double[lN];
+            double[] xArr2 = new double[lN];
             double[] xRe = new double[lN];
             double[] xIm = new double[lN];
             double[] aArr = new double[lN];
+            double[] window = new double[lN];
+            double[] aWind = new double[lN];
 
-            for (int i = 0; i < lN; i++, t += dt)
+            for (int i = 0; i < xArr.Length; i++, t += 1e-3)
             {
                 xArr[i] = func(t);
             }
+            //printArray(xArr);
 
-            getReIm(xRe, xIm, xArr);
-            //printTwoArrays(xRe, xIm);
-
-            for (int i = 0; i < lN; i++, t += dt)
+            for (int n = -window.Length / 2, i = 0; n < window.Length / 2; i++, n++)
             {
-                aArr[i] = getA(xRe[i], xIm[i]);
+                window[i] = getWindowKB(n, lN);
+                xArr[i] *= window[i];
             }
-            printArray(aArr);
-            aArr[9] = 0;
+            //printPosArr(window);
+            //printPosArr(aWind);
 
-            drawA(aArr);
+            getDPF(xRe, xIm, xArr);
+            //printReIm(xRe, xIm);
+
+            for (int i = 0; i < aArr.Length; i++)
+            {
+                aArr[i] = getA(xRe[i], xIm[i]); 
+            }
+            //printPosArr(aArr);
+
+            for (int n = -window.Length / 2, i = 0; n < window.Length / 2; i++, n++)
+            {
+                aWind[i] = aArr[i];
+            }
+            //printPosArr(window);
+            //printPosArr(aWind);
+
+            drawA(aArr, window, aWind);
         }
 
-        private double func(double t)
+        public void drawA(double[] aArr, double[] window, double[] aWind)
         {
-            double la = 10.5, lf = 100;
-
-            return la * Math.Sin(2 * Math.PI * lf * t);
+            chart1.Titles.Add("Без применения окна");
+            chart2.Titles.Add("Окно");
+            chart3.Titles.Add("С применением окна");
+            
+            for (int i = 0; i < aArr.Length; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, 10 * Math.Log(aArr[i]));
+                chart2.Series[0].Points.AddXY(i, window[i]);
+                chart3.Series[0].Points.AddXY(i, 10 * Math.Log(aWind[i]));
+            }
         }
 
-        private void getReIm(double[] xRe, double[] xIm, double[] xArr)
+        static double func(double t)
         {
-            int lN = xRe.Length;
-            double temp;
+            int la = 30, lb = 5;
+            double lf = 150.52302, lf2 = 160.35;
+
+            return la * Math.Sin(2 * Math.PI * lf * t) + lb * Math.Sin(2 * Math.PI * lf2 * t);
+        }
+
+        static double getA(double Re, double Im)
+        {
+            return Re * Re + Im * Im;
+        }
+
+        static void getDPF(double[] xRe, double[] xIm, double[] xArr)
+        {
+            int lN = xArr.Length;
 
             for (int k = 0; k < lN; k++)
             {
@@ -66,45 +103,61 @@ namespace task1
 
                 for (int i = 0; i < lN; i++)
                 {
-                    temp = 2 * Math.PI * i * k / lN;
-                    xRe[k] += xArr[i] * Math.Cos(temp);
-                    xIm[k] += -xArr[i] * Math.Sin(temp);
+                    xRe[k] += xArr[i] * Math.Cos(2 * Math.PI * i * k / lN);
+                    xIm[k] += -xArr[i] * Math.Sin(2 * Math.PI * i * k / lN);
                 }
             }
         }
 
-        private double getA(double xRe, double xIm)
+        static double getWindowKB(int n, int N)
         {
-            return Math.Sqrt(xRe * xRe + xIm * xIm);
-        }
-        public void printTwoArrays(double[] arr1, double[] arr2)
-        {
-            for (int i = 0; i < arr1.Length; i++)
-            {
-                Console.WriteLine("{0} \t {1}", arr1[i], arr2[i]);
-            }
+            double la = 3, param = Math.PI * la;
 
+            return getI0(param * Math.Sqrt(1 - Math.Pow((double)2 * n / N, 2))) / getI0(param);
         }
-        
-        public void printArray(double[] arr)
+
+        static double getI0(double x)
+        {
+            int lk = 0;
+            double leps = 1e-5, lx = x / 2, res = 0;
+            double tempXk = 1, tempKf = 1, tempRes;
+
+            do
+            {
+                tempRes = Math.Pow(tempXk / tempKf, 2);
+                res += tempRes;
+
+                lk++;
+                tempXk *= lx;
+                tempKf *= lk;             
+            }
+            while (tempRes > leps);
+
+            return res;
+        }
+
+        static void printArray(double[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.WriteLine(arr[i]);
             }
         }
-        private void drawA(double[] aArr)
+
+        static void printReIm(double[] xRe, double[] xIm)
         {
-            //chart1.ChartAreas["ChartArea1"].AxisY.IsLogarithmic = true;
-            /*for (int i = 0; i < aArr.Length; i++)
+            for (int i = 0; i < xRe.Length; i++)
             {
-                chart1.Series[0].Points.AddXY(i, 10 * Math.Log10(aArr[i]));
-                aArr[i] = 10 * Math.Log10(aArr[i]);
-                Console.WriteLine(Math.Log10(aArr[i]));
-            }*/
-            int[] i1 = new int[aArr.Length];
-            chart1.Series[0].Points.DataBindXY(i1, aArr);
+                Console.WriteLine(string.Join("\t", xRe[i], xIm[i]));
+            }
         }
-       
+
+        static void printPosArr(double[] aArr)
+        {
+            for (int i = 0; i < aArr.Length; i++)
+            {
+                Console.WriteLine(string.Join("\t", i, aArr[i]));
+            }
+        }
     }
 }
--- task1/Properties/AssemblyInfo.cs
+++ task1/Properties/AssemblyInfo.cs
@@ -2,31 +2,31 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанных со сборкой.
-[assembly: AssemblyTitle("task1")]
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("WindowsFormsApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("task1")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("18a67041-5fb5-479b-8da5-ae111532b188")]
+[assembly: Guid("ccebf2dc-9307-48b4-8f35-bf65421e886d")]
 
-// Сведения о версии сборки состоят из указанных ниже четырех значений:
+// Сведения о версии сборки состоят из следующих четырех значений:
 //
 //      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
+//      Дополнительный номер версии 
+//   Номер сборки
 //      Редакция
 //
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
